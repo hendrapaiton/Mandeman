b65b8fce049b40f972440ea234c3f5ac1071deaf
Hendra Dwi Saputra <hendra@usa.com>
2025-01-27 14:27:15 +0700

    initialize UI project with Vite, React, and Tailwind CSS; add essential configuration files and styles

---
+++ Ui/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-xl">
+          <i className="bi bi-android me-3"></i>
+          Sphinx of black quartz, judge my vow.
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
